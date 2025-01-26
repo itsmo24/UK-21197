cc48c3c4ff10d7b8aa3636127916b8af5ef1eacb
itsmo24 <Muhammad.merchant12@gmail.com>
2025-01-26 19:09:13 +0000

    Made one controls teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MainControls.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/MainControls.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.Teleop.team182;
+package org.firstinspires.ftc.teamcode.Teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-@TeleOp(name = "MainControls182")
+@TeleOp(name = "Main Controls")
 public class MainControls extends LinearOpMode {
     @Override
     public void runOpMode(){
@@ -23,7 +23,7 @@ public class MainControls extends LinearOpMode {
 
         double gripperClosedPosition = 1.0;
         double gripperOpenPosition = 0;
-        boolean isPressed = false;
+        int armHomePosition = 0;
 
         frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
@@ -68,23 +68,18 @@ public class MainControls extends LinearOpMode {
 
             // HANGING BUTTON
             if (gamepad2.dpad_up){
-                rightArm.setPower(-1);
-                leftArm.setPower(-1);
-                sleep(4000);
-                rightArm.setPower(-0.2);
-                leftArm.setPower(-0.2);
+                rightArm.setPower(rightArm.getPower());
+                leftArm.setPower(leftArm.getPower());
             }
 
             //GRIPPER
             // Checks to see if has been pressed before and stops if it has.
-            if (!isPressed && gamepad2.right_bumper){
-                if (gripper.getPosition() == gripperOpenPosition){
-                    gripper.setPosition(gripperClosedPosition);
-                } else{
-                    gripper.setPosition(gripperOpenPosition);
-                }
+            if (gamepad2.right_bumper) {
+                gripper.setPosition(gripperClosedPosition);
+            } else {
+                gripper.setPosition(gripperOpenPosition);
             }
-            isPressed = gamepad2.right_bumper;
+
 
 
             telemetry.addData("Status", "Run Time: " + runtime);
