5a0069b1c902cf2af9c45848ad0f39ec72a3fbbd
Milly134 <carashahimilly@gmail.com>
2025-02-14 00:18:55 +0000

    encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/mainControls004.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/mainControls004.java
@@ -62,7 +62,7 @@ public class mainControls004 extends LinearOpMode {
         //32767/maxvelocity
         //https://ftctechnh.github.io/ftc_app/doc/javadoc/com/qualcomm/robotcore/hardware/DcMotorEx.html#getVelocity-org.firstinspires.ftc.robotcore.external.navigation.AngleUnit-
 //https://docs.google.com/document/d/1tyWrXDfMidwYyP_5H4mZyVgaEswhOC35gvdmP-V-5hA/edit?tab=t.0#heading=h.h2mitzlvr4py
-
+//https://github.com/NoahBres/VelocityPIDTuningTutorial/blob/master/README.md
 
 
 
